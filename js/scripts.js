a2c7537100913b72a595813f5f08ccad1374ccb4
Beth Foster and Chris Berry <beth@techgir1.com>
2016-01-14 16:35:50 -0800

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,9 @@
-$(function () {
-  $('.form').submit(function(event) {
-    // $("").show();
-    // $("").hide();
-  });
-// });
-// exampleRadios
-// $(function(event){
-//   $("#").click(function(){
-// var job = $ ("input#job1").val();
-var characterInput = $("input [name=character]:checked").val();
-  var rachel = 0;
-  counter = 0
-  if (characterInput === "rachel"){
-    counter ++
-  }
+$(document).ready(function() {
+  $('#blanks form').submit(function(event) {
 
-  if (counter > 3) {
-
-  }
-
-    // var rachel = $("").val();
-    // var ross = $("").val();
-    // var phoebe = $("").val();
-    // var chandler = $("").val();
-    // var joey = $("").val();
-
-// var character = ["rachel","ross","phoebe","chandler","joey"];
-//   character.forEach(function(){
-//   });
-
-
-
-    // if ("exampleRadios1"=== "option1" && "exampleRadios9" === "option9") {
-    //   $('rachel').show();
-    // } else if ( === "" &&  === "") {
-    //   $('').show();
-    // } else if ( === "" &&  === "") {
-    //   $('').show();
-    // } else {
-    //   $('').show();
-    // }
-    //
-    // $("").hide();
-    // $("").show();
-
-    event.preventDefault();
-  });
-// });
-
-      // age = parseInt($("input#age1").val());
-      // orientation = $("select#orientation1").val();
-      // gender = $("select#gender1").val();
+      age = parseInt($("input#age1").val());
+      orientation = $("select#orientation1").val();
+      gender = $("select#gender1").val();
 
       // if (age >=50 && gender === "Female" && orientation === "Straight" || gender === "Male" && orientation === "Gay") {
       //   $('#container-result1').show();
@@ -65,3 +18,47 @@ var characterInput = $("input [name=character]:checked").val();
       //     $('#container-result4').show();
         // }
       // }
+
+
+      // }
+
+      if (age >= 50){
+        if (gender === "Female"){
+          if (orientation === "Straight") {
+            $('#container-result2').show();
+          }
+          else {
+            $('#container-result1').show();
+          }
+        }
+        else {
+          if (orientation === "Straight"){
+            $('#container-result1').show();
+          }
+          else {
+            $('#container-result2').show();
+          }
+        }
+      }
+      else {
+        if (gender === "Female"){
+            if (orientation === "Straight"){
+              $('#container-result3').show();
+            }
+            else{
+              $('#container-result4').show();
+            }
+        }
+        else{
+          if (orientation === "Straight") {
+            $('#container-result4').show();
+          }
+          else {
+            $('#container-result3').show();
+          }
+        }
+      }
+
+    event.preventDefault();
+  });
+});
